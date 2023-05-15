21846f60e9b2764ca237f98d16d8d8ec56cc323a
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-05-15 09:52:44 +0300

    fixed bugs

--- lib/features/home/bloc/home_bloc_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'home_bloc_bloc.dart';
-
-@immutable
-abstract class HomeBlocState {}
-
-class HomeBlocInitial extends HomeBlocState {}
--- lib/features/wishlist/bloc/wishlist_bloc_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'wishlist_bloc_bloc.dart';
-
-@immutable
-abstract class WishlistBlocState {}
-
-class WishlistBlocInitial extends WishlistBlocState {}
