7512b560c211cb8a9ce881d500c4e14bd87fa5d0
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-05-15 09:39:55 +0300

    basic structure

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,39 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': '1',
+      'name': 'Bananas',
+      'description': 'Вкусные, сладкие',
+      'price': 0.49,
+      'imageUrl': 'https://fikiwiki.com/uploads/posts/2022-02/1645052523_1-fikiwiki-com-p-kartinki-banani-1.jpg'
+    },
+    {
+      'id': '2',
+      'name': 'Strawberry',
+      'description': 'Ягоды сочные сладкие',
+      'price': 2.19,
+      'imageUrl': 'https://i.artfile.ru/2281x1520_1332915_[www.ArtFile.ru].jpg'
+    },
+    {
+      'id': '3',
+      'name': 'Watermelon',
+      'description': 'Сочный, сладкий',
+      'price': 1.75,
+      'imageUrl': 'https://klike.net/uploads/posts/2022-10/1665573280_3-48.jpg'
+    },
+    {
+      'id': '4',
+      'name': 'Grapes',
+      'description': 'Круглый, сладкий, без косточек',
+      'price': 0.25,
+      'imageUrl': 'https://mobimg.b-cdn.net/v3/fetch/9d/9d5c0e15fe3c3fe3a84e7a7d6a7b69ac.jpeg'
+    },
+    {
+      'id': '5',
+      'name': 'Oranges',
+      'description': 'Ароматные, сладкие, сочные',
+      'price': 1.39,
+      'imageUrl': 'https://pro-dachnikov.com/uploads/posts/2021-11/1638043717_141-pro-dachnikov-com-p-apelsin-foto-153.jpg'
+    },
+  ];
+}
